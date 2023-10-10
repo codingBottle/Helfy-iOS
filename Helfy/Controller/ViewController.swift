ec0dd540cd56957a546b1e9ee652bf1dbc55f0f5
Yun, Seongeun <0420yun@gamil.com>
2023-10-10 20:52:32 +0900

    [#5] feat: 퀴즈뷰 상단탭

--- Helfy/Controller/ViewController.swift
+++ Helfy/Controller/ViewController.swift
@@ -2,18 +2,12 @@
 //  ViewController.swift
 //  Helfy
 //
-//  Created by 김하은 on 2023/10/04.
+//  Created by 윤성은 on 2023/10/06.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
 
 }
 
--- Helfy/SceneDelegate.swift
+++ Helfy/SceneDelegate.swift
@@ -11,15 +11,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let mainViewController = ViewController()
+        let mainViewController = QuizViewController()
 
         window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
     }
+
+
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
