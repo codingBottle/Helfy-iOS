3922d5e2cdf792b2c86296f13766e938a6b46ef4
Yun, Seongeun <0420yun@gamil.com>
2023-10-11 16:03:52 +0900

    [#5] feat : view추가

---
+++ Helfy/Model/QuizModel.swift
@@ -0,0 +1,14 @@
+//
+//  QuizModel.swift
+//  Helfy
+//
+//  Created by 윤성은 on 10/11/23.
+//
+
+import Foundation
+
+
+struct QuizModel {
+    var name: String
+    var score: Int
+}
---
+++ Helfy/View/QuizView.swift
@@ -0,0 +1,19 @@
+//
+//  QuizView.swift
+//  Helfy
+//
+//  Created by 윤성은 on 10/11/23.
+//
+
+import UIKit
+
+class QuizView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
