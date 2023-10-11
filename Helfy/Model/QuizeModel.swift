f82e0307598b9a98fee21657bf908497db4eed14
hanni66 <haeunkim0807@gmail.com>
2023-10-11 16:12:06 +0900

    fix : merge error - quiz view 추가

---
+++ Helfy/Model/QuizeModel.swift
@@ -0,0 +1,12 @@
+//
+//  QuizeModel.swift
+//  Helfy
+//
+//  Created by 김하은 on 2023/10/11.
+//
+
+import UIKit
+
+struct QuizeModel {
+    var name: String
+}
---
+++ Helfy/View/QuizView.swift
@@ -0,0 +1,19 @@
+//
+//  QuizView.swift
+//  Helfy
+//
+//  Created by 김하은 on 2023/10/11.
+//
+
+import UIKit
+
+class QuizView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
