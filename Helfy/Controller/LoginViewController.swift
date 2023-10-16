436c78e75ba6412637b49464ebd626a7ce2c6833
hanni66 <haeunkim0807@gmail.com>
2023-10-16 13:54:19 +0900

    [#13] feat : idToken 서버로 보내기

--- Helfy/Controller/LoginViewController.swift
+++ Helfy/Controller/LoginViewController.swift
@@ -59,7 +59,7 @@ extension LoginViewController {
         // Create Google Sign In configuration object.
         let config = GIDConfiguration(clientID: clientID)
         
-        // Start the sign in flow!
+        // Sign in Flow
         GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
             
             if let error = error {
@@ -71,12 +71,14 @@ extension LoginViewController {
                   let idToken = user.idToken?.tokenString else { return }
             let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
             
+            // firebase
             self.firebaseLogin(credential)
         }
         
     }
     
     func firebaseLogin(_ credential: AuthCredential) {
+        // Firebase Auth
         Auth.auth().signIn(with: credential) { (authResult, error) in
             if let error = error {
                 print("Failed to login with Firebase: ", error)
@@ -86,8 +88,61 @@ extension LoginViewController {
             guard let user = authResult?.user else { return }
             let emailAddress = user.email ?? ""
             let fullName = user.displayName ?? ""
+            let idToken = user.refreshToken ?? ""
             self.textLabel.text = "Hi \(fullName)"
         }
+        
+        // idToken Refresh
+        Auth.auth().currentUser?.getIDTokenForcingRefresh(true, completion: { [self] (idToken, error) in
+            if let error = error {
+                print("Error getting ID token:", error)
+                return
+            }
+            
+            guard let idToken = idToken else {
+                print("ID token is nil")
+                return
+            }
+            
+            sendIDTokenToServer(idToken)
+        })
+    }
+    
+    func sendIDTokenToServer(_ idToken: String) {
+        guard let url = URL(string: "https://helfy-server.duckdns.org/categories") else { return }
+        
+        var request = URLRequest(url: url)
+        
+        request.addValue("Bearer \(idToken)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if let error = error {
+                print("Error sending ID token to server:", error)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                print("Server returned an error")
+                return
+            }
+            
+            if let mimeType = httpResponse.mimeType,
+               mimeType == "application/json",
+               let data = data {
+                
+                do {
+                    // 일단 Json 형식으로 출력 되게끔 작성
+                    let jsonDict = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(jsonDict)
+                } catch {
+                    print("Error parsing JSON:", error)
+                }
+            }
+        }
+        
+        task.resume()
     }
 }
 
