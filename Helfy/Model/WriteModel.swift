27e8807a80cb3d1b0a50e4f670b3eb546c6f0330
Surviveyeomi <yeomi189@gmail.com>
2023-10-17 21:06:01 +0900

    [#14] feat : 네비게이션 버튼 구현

---
+++ Helfy/Model/WriteModel.swift
@@ -0,0 +1,10 @@
+//
+//  WriteModel.swift
+//  Helfy
+//
+//  Created by YEOMI on 10/16/23.
+//
+
+struct MyModel {
+    let text: String
+}
---
+++ Helfy/View/WriteView.swift
@@ -0,0 +1,20 @@
+//
+//  WriteView.swift
+//  Helfy
+//
+//  Created by YEOMI on 10/13/23.
+//
+
+import UIKit
+
+class WriteView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
