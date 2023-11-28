0d736bfe41b6a823f0f2caf04b4586446bd2bec3
Surviveyeomi <yeomi189@gmail.com>
2023-11-29 00:39:36 +0900

    To move branches

--- Helfy/View/BannerView.swift
+++ Helfy/View/BannerView.swift
@@ -4,5 +4,127 @@
 //
 //  Created by YEOMI on 11/18/23.
 //
+import UIKit
 
-import Foundation
+class BannerViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    let collectionView = UICollectionView(frame: CGRect(x: 50, y: 100, width: 300, height: 100), collectionViewLayout: UICollectionViewFlowLayout())
+    let pageControl = UIPageControl()
+    let bannerDuration = 3.0
+    var bannerTimer: Timer?
+    var currentIndex: Int = 0
+    let colors: [UIColor] = [UIColor.red, UIColor.green, UIColor.blue]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
+        
+        setupCollectionView()
+        setupPageControl()
+        startBannerTimer()
+    }
+    
+    func setupCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(BannerCell.self, forCellWithReuseIdentifier: "BannerCell")
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        view.addSubview(collectionView)
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        collectionView.collectionViewLayout = layout
+        
+        collectionView.reloadData()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleBannerTap))
+        collectionView.addGestureRecognizer(tapGesture)
+    }
+    
+    func setupPageControl() {
+        pageControl.frame = CGRect(x: 50, y: 170, width: 300, height: 100) // 배너 아래에 위치
+        pageControl.numberOfPages = colors.count
+        pageControl.currentPage = currentIndex
+        pageControl.isUserInteractionEnabled = false
+        pageControl.currentPageIndicatorTintColor = UIColor(red: 249/255, green: 164/255, blue: 86/255, alpha: 1.0) // 현재 페이지 색상 설정
+        pageControl.pageIndicatorTintColor = UIColor(red: 249/255, green: 223/255, blue: 86/255, alpha: 1.0)
+        
+        view.addSubview(pageControl)
+    }
+    
+    func startBannerTimer() {
+        bannerTimer = Timer.scheduledTimer(timeInterval: bannerDuration, target: self, selector: #selector(updateBanner), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateBanner() {
+        currentIndex = (currentIndex + 1) % colors.count
+        collectionView.scrollToItem(at: IndexPath(item: currentIndex, section: 0), at: .centeredHorizontally, animated: true)
+    }
+    
+    @objc func handleBannerTap() {
+        let currentColor = colors[currentIndex]
+        print("Current Color:", currentColor)
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as! BannerCell
+        let color = colors[indexPath.item] // 해당 셀의 배너 색상
+        cell.configure(with: color) // 색상 파라미터 추가
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegateFlowLayout
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        updateCurrentIndex()
+    }
+    
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        updateCurrentIndex()
+    }
+    
+    private func updateCurrentIndex() {
+        let visibleRect = CGRect(origin: collectionView.contentOffset, size: collectionView.bounds.size)
+        let visiblePoint = CGPoint(x: visibleRect.midX, y: visibleRect.midY)
+        
+        if let indexPath = collectionView.indexPathForItem(at: visiblePoint) {
+            currentIndex = indexPath.item
+            pageControl.currentPage = currentIndex
+        }
+    }
+}
+
+class BannerCell: UICollectionViewCell {
+    var bannerView: UIView = UIView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupBannerView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupBannerView()
+    }
+
+    private func setupBannerView() {
+        bannerView.frame = contentView.bounds
+        bannerView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        contentView.addSubview(bannerView)
+    }
+
+    func configure(with color: UIColor) {
+        bannerView.backgroundColor = color
+    }
+}
--- Helfy/View/SearchView.swift
+++ Helfy/View/SearchView.swift
@@ -4,5 +4,93 @@
 //
 //  Created by YEOMI on 11/21/23.
 //
+import UIKit
 
-import Foundation
+class SearchViewController: UIViewController {
+    
+    let searchContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = 15.0
+        view.layer.backgroundColor = CGColor(red: 249/255, green: 223/255, blue: 86/255, alpha: 1.0)
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    let searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "검색어를 입력하세요"
+        textField.textColor = .black
+        textField.borderStyle = .none
+        return textField
+    }()
+    
+    let searchButtonContainer: UIView = {
+        let container = UIView()
+        container.backgroundColor = UIColor(red: 249/255, green: 164/255, blue: 86/255, alpha: 1.0)
+        container.layer.cornerRadius = 22.5 // 반지름을 버튼 높이의 절반으로 설정
+        return container
+    }()
+    
+    let searchButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        button.tintColor = .black
+        button.addTarget(SearchViewController.self, action: #selector(searchButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        setupUI()
+    }
+    
+    func setupUI() {
+        view.addSubview(searchContainerView)
+        searchContainerView.addSubview(searchTextField)
+        view.addSubview(searchButtonContainer)
+        searchButtonContainer.addSubview(searchButton)
+        
+        searchContainerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            searchContainerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            searchContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            searchContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -80),
+            searchContainerView.heightAnchor.constraint(equalToConstant: 45)
+        ])
+        
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            searchTextField.topAnchor.constraint(equalTo: searchContainerView.topAnchor),
+            searchTextField.leadingAnchor.constraint(equalTo: searchContainerView.leadingAnchor, constant: 10),
+            searchTextField.trailingAnchor.constraint(equalTo: searchContainerView.trailingAnchor, constant: -10),
+            searchTextField.bottomAnchor.constraint(equalTo: searchContainerView.bottomAnchor)
+        ])
+        
+        searchButtonContainer.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            searchButtonContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            searchButtonContainer.leadingAnchor.constraint(equalTo: searchContainerView.trailingAnchor, constant: 10),
+            searchButtonContainer.widthAnchor.constraint(equalToConstant: 45),
+            searchButtonContainer.heightAnchor.constraint(equalToConstant: 45)
+        ])
+        
+        searchButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            searchButton.centerXAnchor.constraint(equalTo: searchButtonContainer.centerXAnchor),
+            searchButton.centerYAnchor.constraint(equalTo: searchButtonContainer.centerYAnchor),
+            searchButton.widthAnchor.constraint(equalTo: searchButtonContainer.widthAnchor),
+            searchButton.heightAnchor.constraint(equalTo: searchButtonContainer.heightAnchor)
+        ])
+    }
+    
+    @objc func searchButtonTapped() {
+        if let searchTerm = searchTextField.text, !searchTerm.isEmpty {
+            print("검색어: \(searchTerm)")
+            // 여기에 실제 검색 동작을 수행하는 코드를 추가할 수 있습니다.
+        } else {
+            print("검색어를 입력하세요.")
+        }
+    }
+}
