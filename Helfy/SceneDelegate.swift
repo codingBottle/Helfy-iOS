3251cacd9d76e5ff91e3bbb78e77af200e7e4c8e
Yun, Seongeun <0420yun@gamil.com>
2023-11-30 01:22:24 +0900

    [#35] feat : 객관식, O/X 퀴즈 뷰 구현

--- Helfy/SceneDelegate.swift
+++ Helfy/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = LoginViewController() // 첫 화면으로 표시할 ViewController 지정
+        window.rootViewController = LoginViewController()
         self.window = window
         window.makeKeyAndVisible()
        
