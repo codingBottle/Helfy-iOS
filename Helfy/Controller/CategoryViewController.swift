e0d0f959d5c62452dbb15209571be7577c981979
Yun, Seongeun <0420yun@gamil.com>
2023-12-18 23:10:32 +0900

    [#16] feat : 버튼 정렬 및 페이징 구현

--- Helfy/Controller/CategoryViewController.swift
+++ Helfy/Controller/CategoryViewController.swift
@@ -9,38 +9,92 @@ import UIKit
 
 class CategoryViewController: UIViewController {
     var categoryView: CategoryView!
+    
+    let pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.numberOfPages = 3 // 총 페이지 수 설정
+        pc.currentPage = 0 // 현재 페이지 설정
+        pc.pageIndicatorTintColor = .gray
+        pc.currentPageIndicatorTintColor = .black
+        return pc
+    }()
+    
+    @objc func handlePageControl(_ sender: UIPageControl) {
+        let currentPage = sender.currentPage
+        let indexPath = IndexPath(item: currentPage * 9, section: 0)
+        categoryView.scrollToItem(at: indexPath, at: .centeredVertically, animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        layout.minimumLineSpacing = 5
-        layout.minimumInteritemSpacing = 5
-        
-        // 셀의 높이를 계산합니다. 여기서는 임의로 100을 사용했습니다.
-        let cellHeight: CGFloat = 100
-        // 셀이 세로로 3개 들어가려면, 셀 높이 * 3 + 위아래 스페이싱 * 2 만큼의 높이가 필요합니다.
-        let collectionViewHeight = cellHeight * 3 + layout.minimumLineSpacing * 2
-        categoryView = CategoryView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: collectionViewHeight), collectionViewLayout: layout)
-        
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+
+        categoryView = CategoryView(frame: .zero, collectionViewLayout: layout)
+        categoryView.isPagingEnabled = true
         categoryView.dataSource = categoryView
-        categoryView.delegate = categoryView
+        categoryView.delegate = self
 
         self.view.addSubview(categoryView)
+        view.addSubview(pageControl)
         view.backgroundColor = .white
 
         setupUI()
+        
+        pageControl.addTarget(self, action: #selector(handlePageControl), for: .valueChanged)
+
     }
 
     private func setupUI() {
         categoryView.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+
+        // Calculate the height of the collectionView
+        let cellHeight = view.frame.width / 3
+        let collectionViewHeight = cellHeight * 3
 
         NSLayoutConstraint.activate([
             categoryView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            categoryView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            categoryView.heightAnchor.constraint(equalToConstant: collectionViewHeight),
             categoryView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            categoryView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            categoryView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            pageControl.topAnchor.constraint(equalTo: categoryView.bottomAnchor, constant: 10),
+//            pageControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
+    }
+}
+
+extension CategoryViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 3
+//        let height: CGFloat = 100 // 높이를 100으로 변경
+        return CGSize(width: width, height: width)
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedCategory = Category.allCases[indexPath.item]
+        print("\(selectedCategory.rawValue) was tapped!")
+
+        // CategoryPage 뷰 컨트롤러를 생성합니다.
+//         let categoryPageViewController = CategoryPageViewController(category: selectedCategory.rawValue)
+        
+        // UINavigationController를 생성하고, rootViewController로 categoryPageViewController를 설정합니다.
+//        let navController = UINavigationController(rootViewController: categoryPageViewController)
+
+        // 생성한 UINavigationController를 모달로 표시합니다.
+//        self.present(navController, animated: true, completion: nil)
+    }
+}
+
+extension CategoryViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x / view.frame.width)
+        pageControl.currentPage = Int(pageIndex)
     }
 }
