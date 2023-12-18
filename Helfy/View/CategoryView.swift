2aed9c2da2f4e8e87c7d269e0993c54ac936a9d3
Yun, Seongeun <0420yun@gamil.com>
2023-12-18 23:10:32 +0900

    [#16] fix : conflict fix

--- Helfy/View/CategoryView.swift
+++ Helfy/View/CategoryView.swift
@@ -9,69 +9,71 @@
 import UIKit
 
 enum Category: String, CaseIterable {
-    case category1 = "침수"
-    case category2 = "태풍"
-    case category3 = "호우"
-    case category4 = "낙뢰"
-    case category5 = "강풍"
-    case category6 = "풍랑"
-    case category7 = "대설"
-    case category8 = "한파"
-    case category9 = "폭염"
-    case category10 = "황사"
-    case category11 = "지진"
-    case category12 = "해일"
-    case category13 = "지진해일"
-    case category14 = "화산폭발"
-    case category15 = "가뭄"
-    case category16 = "홍수"
-    case category17 = "해수면상승"
-    case category18 = "산사태"
-    case category19 = "자연우주물체추락"
-    case category20 = "우주전파재난"
-    case category21 = "녹조"
-    case category22 = "적조"
+    case category1 = "Drought"
+    case category2 = "Strong wind"
+    case category3 = "Lightning"
+    case category4 = "Green tide"
+    case category5 = "Heavy snow"
+    case category6 = "Landslide"
+    case category7 = "Red tide"
+    case category8 = "Earthquake"
+    case category9 = "Earthquake and tsunami"
+    case category10 = "Flooding"
+    case category11 = "Typhoon"
+    case category12 = "Heat wave"
+    case category13 = "Rough sea"
+    case category14 = "Cold wave"
+    case category15 = "Sea level rise"
+    case category16 = "Tsunami"
+    case category17 = "Flood"
+    case category18 = "Yellow dust"
+    case category19 = "Heavy rain"
+    case category20 = "Volcanic eruption"
+    case category21 = "Space radio disaster"
+    case category22 = "Natural space object crash"
+    
+    static func from(string: String) -> Category? {
+        return Category.allCases.first { $0.rawValue == string }
+    }
 }
 
 // Custom UICollectionViewCell
 class CategoryCell: UICollectionViewCell {
     static let identifier = "CategoryCell"
-
-    private let myImageView: UIImageView = {
+    let myImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
 
-    private let myTitleLabel: UILabel = {
+    let myTitleLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 12)
         label.textAlignment = .center
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
 
         contentView.addSubview(myImageView)
         contentView.addSubview(myTitleLabel)
-        
-        myImageView.isUserInteractionEnabled = false
-        myTitleLabel.isUserInteractionEnabled = false
-
 
         NSLayoutConstraint.activate([
+            contentView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            contentView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
             myImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            myImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            myImageView.widthAnchor.constraint(equalToConstant: 90),
-            myImageView.heightAnchor.constraint(equalToConstant: 60),
+            myImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            myImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            myImageView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.5),
 
-            myTitleLabel.topAnchor.constraint(equalTo: myImageView.bottomAnchor, constant: 5),
+            myTitleLabel.topAnchor.constraint(equalTo: myImageView.bottomAnchor, constant: 10),
             myTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            myTitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            myTitleLabel.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor)
+            myTitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
         ])
     }
 
@@ -85,22 +87,18 @@ class CategoryCell: UICollectionViewCell {
     }
 }
 
-// Collection View
-class CategoryView: UICollectionView, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-    private var buttonTitles = ["침수", "태풍", "호우","낙뢰", "강풍", "풍랑", "대설", "한파", "폭염", "황사", "지진", "해일", "지진해일", "화산폭발", "가뭄","홍수", "해수면상승", "산사태","자연우주물체추락", "우주전파재난", "녹조", "적조"]
-    private var buttonImages = [UIImage(named:"침수"), UIImage(named:"태풍"), UIImage(named: "호우"), UIImage(named:"낙뢰"), UIImage(named:"강풍"), UIImage(named: "풍랑"), UIImage(named:"대설"), UIImage(named:"한파"), UIImage(named:"폭염"), UIImage(named:"황사"), UIImage(named:"지진"), UIImage(named:"해일"), UIImage(named:"지진해일"), UIImage(named:"화산폭발"), UIImage(named:"가뭄"), UIImage(named:"홍수"), UIImage(named:"해수면상승"), UIImage(named:"산사태"), UIImage(named:"자연우주물체추락"), UIImage(named:"우주전파재난"), UIImage(named:"녹조"), UIImage(named:"적조")]
 
+// Collection View
+class CategoryView: UICollectionView, UICollectionViewDataSource {
+    var buttonTitles = ["가뭄", "강풍", "낙뢰", "녹조", "대설", "산사태", "적조", "지진", "지진해일", "침수", "태풍", "폭염", "풍랑", "한파", "해수면상승", "해일", "홍수", "황사", "호우", "화산폭발", "우주전파재난", "자연우주물체추락"]
+   
+    var buttonImages = [UIImage(named: "Drought"), UIImage(named: "Strong wind"), UIImage(named:"Lightning"), UIImage(named:"Green tide"), UIImage(named:"Heavy snow"), UIImage(named:"Landslide"), UIImage(named:"Red tide"), UIImage(named:"Earthquake"), UIImage(named:"Earthquake and tsunami"), UIImage(named:"Flooding"), UIImage(named:"Typhoon"), UIImage(named:"Heat wave"), UIImage(named:"Rough sea"), UIImage(named:"Cold wave"), UIImage(named:"Sea level rise"), UIImage(named:"Tsunami"), UIImage(named:"Flood"), UIImage(named:"Yellow dust"), UIImage(named:"Heavy rain"), UIImage(named:"Volcanic eruption"), UIImage(named:"Space radio disaster"), UIImage(named:"Natural space object crash")]
 
     override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
         super.init(frame: frame, collectionViewLayout: layout)
-        
-        if let flowLayout = layout as? UICollectionViewFlowLayout {
-            flowLayout.scrollDirection = .horizontal
-        }
 
         self.register(CategoryCell.self, forCellWithReuseIdentifier: CategoryCell.identifier)
         self.dataSource = self
-        self.delegate = self
     }
 
     required init?(coder: NSCoder) {
@@ -119,18 +117,5 @@ class CategoryView: UICollectionView, UICollectionViewDataSource, UICollectionVi
         let image = buttonImages[indexPath.row]
         cell.configure(with: image, title: title)
         return cell
-    }
-
-    // MARK: UICollectionViewDelegateFlowLayout
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = (collectionView.frame.width - 15 * 2) / 3
-        return CGSize(width: width, height: width)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let selectedCategory = Category.allCases[indexPath.item]
-        print("\(selectedCategory.rawValue) was tapped!")
-        
     }
 }
