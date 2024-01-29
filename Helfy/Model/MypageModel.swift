5176e773931df0405f316a287fefd7bb7bd87f85
Yun, Seongeun <0420yun@gamil.com>
2024-01-29 23:17:00 +0900

    [#29] fix : 충돌 해

--- Helfy/Model/MypageModel.swift
+++ Helfy/Model/MypageModel.swift
@@ -7,30 +7,34 @@
 
 import UIKit
 
+// MARK: - MypageModel
+struct MypageModel: Codable {
+    let region, nickname: String
+}
+
+
 // User 모델 클래스
-class User2 {
-    var nickname: String
-    var location: String
-    
-    init(nickname: String, location: String) {
-        self.nickname = nickname
-        self.location = location
-    }
-}
-
-class MypageModel {
-    var user: User2
-    
-    init(user: User2) {
-        self.user = user
-    }
-    
-    // myPageView를 사용하여 UI를 업데이트하는 메서드
-    func updateUI() {
-        let myPageView = MypageView()
-        myPageView.nicknameLabel.text = user.nickname
-        myPageView.locationLabel.text = user.location
-    }
-}
-
-
+//class User {
+//    var nickname: String
+//    var location: String
+//    
+//    init(nickname: String, location: String) {
+//        self.nickname = nickname
+//        self.location = location
+//    }
+//}
+//
+//class MypageModel {
+//    var user: User
+//    
+//    init(user: User) {
+//        self.user = user
+//    }
+//    
+//    // myPageView를 사용하여 UI를 업데이트하는 메서드
+//    func updateUI() {
+//        let myPageView = MypageView()
+//        myPageView.nicknameLabel.text = user.nickname
+//        myPageView.locationLabel.text = user.location
+//    }
+//}
