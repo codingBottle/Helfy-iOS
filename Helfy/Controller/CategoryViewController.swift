df8398dd9187774c014893d90fb5477735d0e803
Yun, Seongeun <0420yun@gamil.com>
2024-02-08 20:19:29 +0900

    UI 수정

--- Helfy/Controller/CategoryViewController.swift
+++ Helfy/Controller/CategoryViewController.swift
@@ -7,94 +7,152 @@
 
 import UIKit
 
-class CategoryViewController: UIViewController {
-    var categoryView: CategoryView!
+class CategoryViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    let pageControl: UIPageControl = {
-        let pc = UIPageControl()
-        pc.numberOfPages = 3 // 총 페이지 수 설정
-        pc.currentPage = 0 // 현재 페이지 설정
-        pc.pageIndicatorTintColor = .gray
-        pc.currentPageIndicatorTintColor = .black
-        return pc
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "카테고리"
+        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
     
-    @objc func handlePageControl(_ sender: UIPageControl) {
-        let currentPage = sender.currentPage
-        let indexPath = IndexPath(item: currentPage * 9, section: 0)
-        categoryView.scrollToItem(at: indexPath, at: .centeredVertically, animated: true)
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         layout.minimumLineSpacing = 0
         layout.minimumInteritemSpacing = 0
+//        layout.sectionInset = UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 0)  // 추가된 부분
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(CategoryCell.self, forCellWithReuseIdentifier: "cell")
+        return cv
+    }()
 
-        categoryView = CategoryView(frame: .zero, collectionViewLayout: layout)
-        categoryView.isPagingEnabled = true
-        categoryView.dataSource = categoryView
-        categoryView.delegate = self
-
-        self.view.addSubview(categoryView)
-        view.addSubview(pageControl)
-        view.backgroundColor = .white
-
-        setupUI()
-        
-        pageControl.addTarget(self, action: #selector(handlePageControl), for: .valueChanged)
-
-    }
-
-    private func setupUI() {
-        categoryView.translatesAutoresizingMaskIntoConstraints = false
+    
+    var pageController: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = 3
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = .gray
+        pageControl.currentPageIndicatorTintColor = .darkGray
         pageControl.translatesAutoresizingMaskIntoConstraints = false
-
-        // Calculate the height of the collectionView
-        let cellHeight = view.frame.width / 3
-        let collectionViewHeight = cellHeight * 3
-
+        
+        return pageControl
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        view.addSubview(titleLabel)
+        view.addSubview(collectionView)
+        view.addSubview(pageController)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        
+        pageController.addTarget(self, action: #selector(handlePageControl(_:)), for: .valueChanged)
+        
         NSLayoutConstraint.activate([
-            categoryView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            categoryView.heightAnchor.constraint(equalToConstant: collectionViewHeight),
-            categoryView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            categoryView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
-            pageControl.topAnchor.constraint(equalTo: categoryView.bottomAnchor, constant: 10),
-//            pageControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 40),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            collectionView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
+            
+            pageController.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 30),
+            pageController.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pageController.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            pageController.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
         ])
     }
-}
-
-extension CategoryViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.frame.width / 3
-//        let height: CGFloat = 100 // 높이를 100으로 변경
-        return CGSize(width: width, height: width)
+    
+    @objc func handlePageControl(_ sender: UIPageControl) {
+        let currentPage = sender.currentPage
+        let x = CGFloat(currentPage) * collectionView.frame.width
+        collectionView.setContentOffset(CGPoint(x: x, y: 0), animated: true)
     }
-
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x/collectionView.frame.width)
+        pageController.currentPage = Int(pageIndex)
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+        
+        var offset = targetContentOffset.pointee
+        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+        let roundedIndex = round(index)
+        
+        offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: -scrollView.contentInset.top)
+        targetContentOffset.pointee = offset
+    }
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let selectedCategory = Category.allCases[indexPath.item]
-        print("\(selectedCategory.rawValue) was tapped!")
-
-        // CategoryPage 뷰 컨트롤러를 생성합니다.
-//         let categoryPageViewController = CategoryPageViewController(category: selectedCategory.rawValue)
+        // 선택된 카테고리 가져오기
+        let selectedCategory = Category.allCases[indexPath.row]
+        print("Selected category: \(selectedCategory.rawValue)")
         
-        // UINavigationController를 생성하고, rootViewController로 categoryPageViewController를 설정합니다.
-//        let navController = UINavigationController(rootViewController: categoryPageViewController)
-
-        // 생성한 UINavigationController를 모달로 표시합니다.
-//        self.present(navController, animated: true, completion: nil)
+        
+        // 카테고리 정보 뷰 컨트롤러 생성
+        //        let categoryVC = CategoryPageViewController()
+        //
+        //        // 카테고리 정보 뷰 컨트롤러에 선택된 카테고리 설정
+        //        categoryVC.category = selectedCategory
+        //
+        //        // 카테고리 정보 뷰 컨트롤러 표시
+        //        navigationController?.pushViewController(categoryVC, animated: true)
     }
-}
-
-extension CategoryViewController: UIScrollViewDelegate {
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let pageIndex = round(scrollView.contentOffset.x / view.frame.width)
-        pageControl.currentPage = Int(pageIndex)
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let totalCells = buttonImages.count
+        let remainder = totalCells % 9
+        return totalCells + (remainder > 0 ? (9 - remainder) : 0) // 빈 셀 추가
     }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CategoryCell
+        if indexPath.row < buttonImages.count {
+            cell.categoryView.buttonImageView.image = buttonImages[indexPath.row]
+            cell.categoryView.buttonLabel.text = buttonTitles[indexPath.row]
+        } else {
+            cell.categoryView.buttonImageView.image = nil
+            cell.categoryView.buttonLabel.text = nil
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+//            let totalSpacing = layout.minimumInteritemSpacing * 4
+//            let width = (collectionView.frame.width - totalSpacing) / 3
+//            let height = (collectionView.frame.height - totalSpacing) / 3
+//
+//            let height: CGFloat = 100 // 셀 높이를 원하는 값으로 고정
+//            return CGSize(width: width, height: height)
+//        }
+    
+        let width = collectionView.frame.width / 3
+        let height = collectionView.frame.height / 3
+        return CGSize(width: width, height: height)
+    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let padding: CGFloat = 30 * 2
+//        let sectionInsets: CGFloat = 10 * 2
+//        let cellCount: CGFloat = 3
+//        let width = (collectionView.frame.width - padding - sectionInsets) / cellCount
+//        let height = (collectionView.frame.height - sectionInsets) / 3
+//        return CGSize(width: width, height: height)
+//    }
+
+
 }
--- Helfy/View/CategoryView.swift
+++ Helfy/View/CategoryView.swift
@@ -26,7 +26,7 @@ enum Category: String, CaseIterable {
     case category15 = "Sea level rise"
     case category16 = "Tsunami"
     case category17 = "Flood"
-    case category18 = "Yellow dust"
+    case category18 = "Dust storm"
     case category19 = "Heavy rain"
     case category20 = "Volcanic eruption"
     case category21 = "Space radio disaster"
@@ -37,85 +37,75 @@ enum Category: String, CaseIterable {
     }
 }
 
+var buttonTitles = ["가뭄", "강풍", "낙뢰", "녹조", "대설", "산사태", "적조", "지진", "지진해일", "침수", "태풍", "폭염", "풍랑", "한파", "해수면상승", "해일", "홍수", "황사", "호우", "화산폭발", "우주전파재난", "우주물체추락"]
+
+var buttonImages = [UIImage(named: "Drought"), UIImage(named: "Strong wind"), UIImage(named:"Lightning"), UIImage(named:"Green tide"), UIImage(named:"Heavy snow"), UIImage(named:"Landslide"), UIImage(named:"Red tide"), UIImage(named:"Earthquake"), UIImage(named:"Earthquake and tsunami"), UIImage(named:"Flooding"), UIImage(named:"Typhoon"), UIImage(named:"Heat wave"), UIImage(named:"Rough sea"), UIImage(named:"Cold wave"), UIImage(named:"Sea level rise"), UIImage(named:"Tsunami"), UIImage(named:"Flood"), UIImage(named:"Dust storm"), UIImage(named:"Heavy rain"), UIImage(named:"Volcanic eruption"), UIImage(named:"Space radio disaster"), UIImage(named:"Natural space object crash")]
+
 // Custom UICollectionViewCell
 class CategoryCell: UICollectionViewCell {
-    static let identifier = "CategoryCell"
-    let myImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFit
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-
-    let myTitleLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 12)
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
+    
+    let categoryView: CategoryView = {
+        let cv = CategoryView()
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-
-        contentView.addSubview(myImageView)
-        contentView.addSubview(myTitleLabel)
-
+        
+        contentView.addSubview(categoryView)
+        
         NSLayoutConstraint.activate([
-            contentView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            contentView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            
-            myImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            myImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            myImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            myImageView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.5),
-
-            myTitleLabel.topAnchor.constraint(equalTo: myImageView.bottomAnchor, constant: 10),
-            myTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            myTitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+            categoryView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            categoryView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            categoryView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            categoryView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-
-    func configure(with image: UIImage?, title: String) {
-        myImageView.image = image
-        myTitleLabel.text = title
     }
 }
 
-
-// Collection View
-class CategoryView: UICollectionView, UICollectionViewDataSource {
-    var buttonTitles = ["가뭄", "강풍", "낙뢰", "녹조", "대설", "산사태", "적조", "지진", "지진해일", "침수", "태풍", "폭염", "풍랑", "한파", "해수면상승", "해일", "홍수", "황사", "호우", "화산폭발", "우주전파재난", "자연우주물체추락"]
-   
-    var buttonImages = [UIImage(named: "Drought"), UIImage(named: "Strong wind"), UIImage(named:"Lightning"), UIImage(named:"Green tide"), UIImage(named:"Heavy snow"), UIImage(named:"Landslide"), UIImage(named:"Red tide"), UIImage(named:"Earthquake"), UIImage(named:"Earthquake and tsunami"), UIImage(named:"Flooding"), UIImage(named:"Typhoon"), UIImage(named:"Heat wave"), UIImage(named:"Rough sea"), UIImage(named:"Cold wave"), UIImage(named:"Sea level rise"), UIImage(named:"Tsunami"), UIImage(named:"Flood"), UIImage(named:"Yellow dust"), UIImage(named:"Heavy rain"), UIImage(named:"Volcanic eruption"), UIImage(named:"Space radio disaster"), UIImage(named:"Natural space object crash")]
-
-    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
-        super.init(frame: frame, collectionViewLayout: layout)
-
-        self.register(CategoryCell.self, forCellWithReuseIdentifier: CategoryCell.identifier)
-        self.dataSource = self
+class CategoryView: UIView {
+    
+    let buttonImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    let buttonLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(buttonImageView)
+        addSubview(buttonLabel)
+        
+        NSLayoutConstraint.activate([
+            buttonImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 15),
+            buttonImageView.widthAnchor.constraint(equalTo: widthAnchor),
+            buttonImageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5),
+            
+            buttonLabel.topAnchor.constraint(equalTo: buttonImageView.bottomAnchor, constant: 10),
+            buttonLabel.widthAnchor.constraint(equalTo: widthAnchor),
+            buttonLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-
-    // MARK: UICollectionViewDataSource
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return buttonTitles.count
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCell.identifier, for: indexPath) as! CategoryCell
-        let title = buttonTitles[indexPath.row]
-        let image = buttonImages[indexPath.row]
-        cell.configure(with: image, title: title)
-        return cell
     }
 }
