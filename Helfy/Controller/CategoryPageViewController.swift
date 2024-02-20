9cd569fb1ca04935ac86272158f88d262fc86e2f
Surviveyeomi <yeomi189@gmail.com>
2024-02-20 14:45:43 +0900

    [#72] fix : fix for category-page

--- Helfy/Controller/CategoryPageViewController.swift
+++ Helfy/Controller/CategoryPageViewController.swift
@@ -4,28 +4,49 @@
 //
 //  Created by YEOMI on 11/29/23.
 //
+
 import UIKit
 
 class CategoryPageViewController: UIViewController {
     
-    let categoryPageViewInstance = CategoryPageView()
+    var categoryPageView = CategoryPageView()
+    let apiHandler = CategoryPageAPIHandler()
+    var presentCategory: String?
 
-    lazy var newsURL = categoryPageViewInstance.newsURL
-    lazy var youtubeURL = categoryPageViewInstance.youtubeURL
+    lazy var newsURL = categoryPageView.newsURL
+    lazy var youtubeURL = categoryPageView.youtubeURL
     
-    private var categoryPageView: CategoryPageView!
+    var categoryPageModelData: CategoryPageModel? {
+        didSet {
+            print("hi")
+        }
+    }
+    
+    var categoryPageData: CategoryPageModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        categoryPageView = CategoryPageView(frame: view.frame)
+        view.addSubview(categoryPageView)
+        
+        setData()
         setupUI()
+        
+        categoryPageView.newsButton.addTarget(self, action: #selector(newsButtonTapped), for: .touchUpInside)
+        categoryPageView.youtubeButton.addTarget(self, action: #selector(youtubeButtonTapped), for: .touchUpInside)
     }
+
     
     private func setupUI() {
         view.backgroundColor = .white
         
-        categoryPageView = CategoryPageView()
-        categoryPageView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(categoryPageView)
+
+        categoryPageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        print("alpha : \(categoryPageView.alpha)")
+        print("frame : \(categoryPageView.frame)")
         
         NSLayoutConstraint.activate([
             categoryPageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -33,13 +54,44 @@ class CategoryPageViewController: UIViewController {
             categoryPageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             categoryPageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
-        
-        categoryPageView.setNewsButtonTarget(self, action: #selector(newsButtonTapped))
-        categoryPageView.setYouTubeButtonTarget(self, action: #selector(youtubeButtonTapped))
     }
     
-    
-    
+    func setData() {
+        DispatchQueue.global(qos: .userInteractive).async {
+            guard let presentCategory = self.presentCategory else { return }
+
+            // API 통해 데이터 불러오기
+            self.apiHandler.getCategoryPageData(category: presentCategory) { [weak self] data in
+                guard let self = self else { return }
+                // 정의해둔 모델 객체에 할당
+                self.categoryPageData = data
+                
+                // 데이터를 제대로 잘 받아왔다면
+                guard let data = self.categoryPageData else {
+                    return print("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥")
+                }
+                
+                DispatchQueue.main.async {
+                    self.categoryPageView.categoryLabel.text = data.category
+                    self.categoryPageView.contentView.text = data.content
+                    self.categoryPageView.newsURL = data.newsURL
+                    self.categoryPageView.youtubeURL = data.youtubeURL
+                }
+                guard let imageUrl = URL(string: data.image.imageURL) else {
+                    print("Invalid URL")
+                    return
+                }
+
+                ImageLoader.loadImage(url: data.image.imageURL) { image in
+                    DispatchQueue.main.async {
+                        self.categoryPageView.categoryImageView.image = image
+                    }
+                }
+
+            }
+            
+        }
+    }
     
     @objc func newsButtonTapped() {
         // Handle news button tap
@@ -48,7 +100,7 @@ class CategoryPageViewController: UIViewController {
             UIApplication.shared.open(url)
         }
     }
-    
+
     @objc func youtubeButtonTapped() {
         // Handle YouTube button tap
         print("YouTube URL:", youtubeURL) //디버깅 용도
